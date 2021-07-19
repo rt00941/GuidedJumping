f6b82f2bb4055beb3e6f6a1de874ae81ce0d2746
rt00941 <ramshasaad@gmail.com>
2021-07-19 15:43:57 +0200

    oculus test scene, nodes

--- Assets/Scripts/GuidedJumping.cs
+++ Assets/Scripts/GuidedJumping.cs
@@ -1,16 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GuidedJumping : MonoBehaviour
 {
-
-    private List<Vector3> nodeslist;
-
+    private GameObject[] nodes;
     // Start is called before the first frame update
     void Start()
     {
-        MakeNodeList();
+        nodes = GameObject.FindGameObjectsWithTag("Node");
+        Debug.Log(nodes.Length);
+        StartCoroutine(jumping());
     }
 
     // Update is called once per frame
@@ -19,8 +20,35 @@ public class GuidedJumping : MonoBehaviour
         
     }
 
-    void MakeNodeList()
+    IEnumerator jumping()
     {
-        foreach (GameObject obj in Game)
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            yield return new WaitForSeconds(2);
+
+            gameObject.transform.SetParent(((GameObject)nodes.GetValue(i)).transform);
+            gameObject.transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void CalculateWaypoints(GameObject startnode, GameObject endnode)
+    {
+        float xdiff = endnode.transform.position.x - startnode.transform.position.x;
+        float ydiff = endnode.transform.position.y - startnode.transform.position.y;
+        float zdiff = endnode.transform.position.z - startnode.transform.position.z;
+
+        float xincrement = 0.1f;
+        float yincrement = 0.1f;
+        float zincrement = 0.1f;
+    }
+
+    private object WaitForSeconds(int v)
+    {
+        throw new NotImplementedException();
+    }
+
+    private object WaitForSecondsRealtime(int v)
+    {
+        throw new NotImplementedException();
     }
 }
