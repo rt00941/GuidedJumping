087df3f7f5e94610d70382fc4adb1fdd3ee51e90
rt00941 <ramshasaad@gmail.com>
2021-08-01 19:11:34 +0200

    gestures 4
    code complete
    have to test and add gestures (fix oculus connection issues first)

--- Assets/Scripts/GestureManager.cs
+++ Assets/Scripts/GestureManager.cs
@@ -15,15 +15,18 @@ public struct Gesture
 
 public class GestureManager : MonoBehaviour
 {
+    public float threshold = 0.1f;
     public OVRSkeleton skeleton;
     public bool debugMode = true;
     public List<Gesture> gestures;
     private List<OVRBone> fingerBones;
+    private Gesture previousGesture;
 
     // Start is called before the first frame update
     void Start()
     {
         fingerBones = new List<OVRBone>(skeleton.Bones);
+        previousGesture = new Gesture();
     }
 
     // Update is called once per frame
@@ -32,6 +35,17 @@ public class GestureManager : MonoBehaviour
         if (debugMode && Input.GetKeyDown(KeyCode.Space))
         {
             Save();
+        }
+
+        Gesture currentGesture = Recognize();
+        bool hasRecognized = !currentGesture.Equals(new Gesture());
+        // check if new gesture
+        if (hasRecognized && !currentGesture.Equals(previousGesture))
+        {
+            // it is a new gesture
+            Debug.Log("New Gesture found: " + currentGesture.gestureName);
+            previousGesture = currentGesture;
+            currentGesture.onRecognized.Invoke();
         }
     }
 
@@ -47,6 +61,36 @@ public class GestureManager : MonoBehaviour
         }
         g.fingersData = data;
         gestures.Add(g);
+    }
+
+    Gesture Recognize()
+    {
+        Gesture currentGesture = new Gesture();
+        float currentMin = Mathf.Infinity;
+
+        foreach(var gesture in gestures)
+        {
+            float sumDistance = 0;
+            bool isDiscarded = false;
+            for (int i=0; i < fingerBones.Count; i++)
+            {
+                Vector3 currentData = skeleton.transform.InverseTransformPoint(fingerBones[i].Transform.position);
+                float distance = Vector3.Distance(currentData, gesture.fingersData[i]);
+                if (distance > threshold)
+                {
+                    isDiscarded = true;
+                    break;
+                }
+                sumDistance += distance;
+
+            }
+            if (!isDiscarded && sumDistance < currentMin)
+            {
+                currentMin = sumDistance;
+                currentGesture = gesture;
+            }
+        }
+        return currentGesture;
     }
 
 }
