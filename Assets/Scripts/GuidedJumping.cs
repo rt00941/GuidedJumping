5ce73f7c58b7b26f40ebf785c0c471f78212ab7b
rt00941 <ramshasaad@gmail.com>
2021-08-03 15:04:00 +0200

    Preview 1
    Added ghost avatar code
    next: multiple options, fix waypoint rotations in scene

--- Assets/Scripts/GuidedJumping.cs
+++ Assets/Scripts/GuidedJumping.cs
@@ -7,6 +7,7 @@ public class GuidedJumping : MonoBehaviour
 {
     private GameObject[] nodes;
     private GameObject currentNode;
+    private GameObject ghostAvatar;
     private Transform currentWaypoints; 
     private Dictionary<int, Dictionary<int, GameObject>> ordered = new Dictionary<int, Dictionary<int, GameObject>>();
     private int chosenNode;
@@ -16,6 +17,7 @@ public class GuidedJumping : MonoBehaviour
     {
         nodes = GameObject.FindGameObjectsWithTag("Node");
         chosenNode = 0;
+        ghostAvatar = GameObject.Find("GhostAvatar");
         orderNodes();
         StartCoroutine(jumping());
     }
@@ -33,12 +35,22 @@ public class GuidedJumping : MonoBehaviour
             currentNode = ordered[i][chosenNode];
             currentWaypoints = currentNode.GetComponent<Node>().thisnode.waypoints.transform;
             for (int j = 0; j < currentWaypoints.childCount; j++)
-            {                
+            {
+                ghostAvatar.transform.parent = currentWaypoints.GetChild(j).transform;
+                ghostAvatar.transform.localPosition = new Vector3(0, -0.7f, 0);
+                ghostAvatar.GetComponent<MeshRenderer>().enabled = true;
                 yield return new WaitForSeconds(2);
+                ghostAvatar.GetComponent<MeshRenderer>().enabled = false;
                 gameObject.transform.position = currentWaypoints.GetChild(j).transform.position;
+                gameObject.transform.rotation = currentWaypoints.GetChild(j).transform.rotation;
             }
+            ghostAvatar.transform.parent = currentNode.transform;
+            ghostAvatar.transform.localPosition = new Vector3(0, -0.7f, 0);
+            ghostAvatar.GetComponent<MeshRenderer>().enabled = true;
             yield return new WaitForSeconds(2);
+            ghostAvatar.GetComponent<MeshRenderer>().enabled = false;
             gameObject.transform.position = currentNode.transform.position;
+            gameObject.transform.rotation = currentNode.transform.rotation;
         }
     }
 
